215c68631faf172eac621b88bf23117e9029db1c
AhiAz <jahiaz@gmail.com>
2022-12-28 10:29:05 -0600

    remove unnecesary files

--- acebind.js
+++ acebind.js
@@ -4,45 +4,8 @@
 
 Simple javascript library to help you to make a two way binding with the dom in easy way with proxy method, and add elements to it with a simple declaration of json.
 
-How to use:
+Created by: Jose Ahias Vargas Pacheco
 
-**first import the library in your class module (test example)**
-
-import {aceBind} from './acebind.js';
-
-**then make a new aceBind instance on constructor class method**
-
-this.ace = new aceBind(); //this will make a bind in all elements with the attribute "ace-bind"
-
-**change element value with 2 way binding**
-
-this.ace.scope.variablename =  "this is a test" //reflects the value in all dom elements with the attribute "ace-bind = variablename"
-
-**add dom elements dynamically**
-
-Method: addEle
-Description: Append new element to the dom with 2 way binding and using json array to create the elements.
-Params: element = is the json array of elements
-        target = an existing htmElement on DOM, where the element will append it
-
-        Example:
-
-        this.ace.addEle(
-        [{class:"col-md-6", key: "divLastname", el:"div"},
-        {class:"form-control", key: "lastname", el:"input", type:"text", acebind:"lastname", parent:"divLastname"},
-        {class:"fw-bold", key: "spanLastname", el:"span", parent:"divLastname", text:"lastname scope: "},
-        {class:"fw-normal text-muted", el:"span", acebind:"lastname", parent:"spanLastname"}
-        ], infoDiv);
-
-__Relevant attributes__:
-
-key: unique id for append dom elements;
-parent: indicates that the current element will be append to the element with the especified parent key
-acebind: variable name to store the 2 way binding
-el: is the html element type, like: div, span, input, label etc...
-text: set a text for the element, like: p, label, span, h1 etc...
-
-other atributes: common values for html elements like: id, class, type, style etc... 
 */
 
 
